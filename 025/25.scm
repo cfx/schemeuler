17ba869d5d9e6c459d1577d6cdacb4fff70f89b5
Jozef Chraplewski <jozef@applicake.com>
2012-05-07 00:27:33 +0200

    problem 25 solved

---
+++ 025/25.scm
@@ -0,0 +1,20 @@
+;; a: 4782
+
+(define problem-25
+  (lambda (len)
+    (call/cc
+     (lambda (brk)
+       (letrec ((fib (lambda (prev n init limit)
+                       (if (eq? init limit)
+                           (+ prev n)
+                           (fib2 n (+ prev n) (+ init 1) limit))))
+                (F (lambda (init)
+                     (if (>= (string-length (number->string (fib 0 1 2 init)))
+                             len)
+                         (brk init)
+                         (F (+ init 1))))))
+         (F 3))))))
+
+
+(display (problem-25 1000))
+(display "\n")
