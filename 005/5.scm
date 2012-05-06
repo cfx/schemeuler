c320f9f5a327bd7307db5ead8cfc1ff7929ea62b
Jozef Chraplewski <jozef@applicake.com>
2012-05-06 20:48:15 +0200

    problem 5 cleaned up

--- 005/5.scm
+++ 005/5.scm
@@ -4,35 +4,27 @@
 ;; What is the smallest positive number that is evenly divisible by all
 ;; of the numbers from 1 to 20?
 
-;; 27720
+;; a: 232792560
 
-(define (divby1? n m)
-  (if (zero? n)
-      #t
-      (and (zero? (remainder m n))
-           (divby1? (- n 1) m))))
 
-(divby1? 20 232792560)
 
-(define (divby? init limit m)
-  (cond
-   ((zero? (remainder m init))
-    (cond
-     ((= init limit) #t)
-     (else (divby? (+ init 1) limit m))))
-   (else #f)))
+(define problem-5
+  (lambda (init limit)
+    (call/cc
+     (lambda (brk)
+       (letrec ((F (lambda (n i)
+                     (cond
+                      ((eq? i limit)
+                       (brk n))
+                      ((zero? (remainder n i))
+                       (F n (+ i 1)))
+                      (else (F (+ n 1) init))))))
+         (F 1 1))))))
 
-(divby? 1 11 2530)
+;; brute-force solution
 
-(define (find-smallest-for n m)
-  (if (divby1? n m)
-      m
-      (find-smallest-for n (+ m 1))))
+(display (problem-5 1 20))
+(display "\n")
 
-(define (final init limit m)
-  (let ((result (find-smallest-for init m)))
-    (if (= init limit)
-        result
-        (final (+ init 1) limit result))))
 
-(final 19 19 24504480)
+
