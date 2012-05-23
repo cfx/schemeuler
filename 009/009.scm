9653b960020ae6be1ae3c494f5fd8894d3b14331
Jozef Chraplewski <jozef@applicake.com>
2012-05-23 11:03:37 +0200

    problem 9 solved

---
+++ 009/009.scm
@@ -0,0 +1,18 @@
+;; a: 31875000
+
+(let ([t-sum 1000])
+  (call/cc
+   (lambda (brk)
+     (let loop-a ([a 1])
+       (begin
+         (let loop-b ([b (+ a 1)])
+           (let ([c2 (+ (expt a 2) (expt b 2))])
+             (let ([sum (+ a b (sqrt c2))])
+               (cond
+                ((> sum t-sum) #f)
+                ((eq? sum t-sum)
+                 (brk (* a b (sqrt c2))))
+                (else (loop-b (+ b 1)))))))
+         (if (> a t-sum)
+             #f
+             (loop-a (+ a 1))))))))
